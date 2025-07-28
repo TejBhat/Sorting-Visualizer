71bbcb0b4c7b5fde46f6d77175fc72a44258034d
Tej M Bhat <tejbhat2004@gmail.com>
2025-07-28 15:31:02 +0530

    Update sorting visualizer project

--- scripts/heap_sort.js
+++ scripts/heap_sort.js
@@ -1,18 +1,13 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
+
 
 function Heap()
 {
-    //Setting Time complexities
+    
     document.getElementById("Time_Worst").innerText="O(N log N)";
     document.getElementById("Time_Average").innerText="Θ(N log N)";
     document.getElementById("Time_Best").innerText="Ω(N log N)";
 
-    //Setting Space complexity
+    
     document.getElementById("Space_Worst").innerText="O(1)";
 
     c_delay=0;
@@ -24,18 +19,18 @@ function Heap()
 
 function swap(i,j)
 {
-    div_update(divs[i],div_sizes[i],"red");//Color update
-    div_update(divs[j],div_sizes[j],"red");//Color update
+    div_update(divs[i],div_sizes[i],"red");
+    div_update(divs[j],div_sizes[j],"red");
 
     var temp=div_sizes[i];
     div_sizes[i]=div_sizes[j];
     div_sizes[j]=temp;
 
-    div_update(divs[i],div_sizes[i],"red");//Height update
-    div_update(divs[j],div_sizes[j],"red");//Height update
+    div_update(divs[i],div_sizes[i],"red");
+    div_update(divs[j],div_sizes[j],"red");
 
-    div_update(divs[i],div_sizes[i],"blue");//Color update
-    div_update(divs[j],div_sizes[j],"blue");//Color update
+    div_update(divs[i],div_sizes[i],"#191970");
+    div_update(divs[j],div_sizes[j],"blue");
 }
 
 function max_heapify(n,i)
@@ -48,24 +43,24 @@ function max_heapify(n,i)
     {
         if(largest!=i)
         {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
+            div_update(divs[largest],div_sizes[largest],"#191970");
         }
 
         largest=l;
 
-        div_update(divs[largest],div_sizes[largest],"red");//Color update
+        div_update(divs[largest],div_sizes[largest],"red");
     }
 
     if(r<n && div_sizes[r]>div_sizes[largest])
     {
         if(largest!=i)
         {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
+            div_update(divs[largest],div_sizes[largest],"#191970");
         }
 
         largest=r;
 
-        div_update(divs[largest],div_sizes[largest],"red");//Color update
+        div_update(divs[largest],div_sizes[largest],"red");
     }
 
     if(largest!=i)
@@ -86,20 +81,14 @@ function heap_sort()
     for(var i=array_size-1;i>0;i--)
     {
         swap(0,i);
-        div_update(divs[i],div_sizes[i],"green");//Color update
-        div_update(divs[i],div_sizes[i],"yellow");//Color update
+        div_update(divs[i],div_sizes[i],"green");
+        div_update(divs[i],div_sizes[i],"yellow");
 
         max_heapify(i,0);
 
-        div_update(divs[i],div_sizes[i],"blue");//Color update
-        div_update(divs[i],div_sizes[i],"green");//Color update
+        div_update(divs[i],div_sizes[i],"#191970");
+        div_update(divs[i],div_sizes[i],"green");
     }
-    div_update(divs[i],div_sizes[i],"green");//Color update
+    div_update(divs[i],div_sizes[i],"green");
 }
 
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,8 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
 
-/*
-Variable naming convention: <object>_<action>_<objectname>; Example -> Button_click_b1;
-*/
-
-//Variables (BE CAREFUL THESE MIGHT BE USED IN OTHER JS FILES TOO)
 var inp_as=document.getElementById('a_size'),array_size=inp_as.value;
 var inp_gen=document.getElementById("a_generate");
 var inp_aspeed=document.getElementById("a_speed");
-//var array_speed=document.getElementById('a_speed').value;
+
 
 var butts_algos=document.querySelectorAll(".algos button");
 
@@ -23,7 +12,7 @@ var margin_size;
 var cont=document.getElementById("array_container");
 cont.style="flex-direction:row";
 
-//Array generation and updation.
+
 
 inp_gen.addEventListener("click",generate_array);
 inp_as.addEventListener("input",update_array_size);
@@ -38,7 +27,7 @@ function generate_array()
         divs[i]=document.createElement("div");
         cont.appendChild(divs[i]);
         margin_size=0.1;
-        divs[i].style=" margin:0% " + margin_size + "%; background-color:blue; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+        divs[i].style=" margin:0% " + margin_size + "%; background-color:#191970; width:" + (100/array_size-(2*margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
     }
 }
 
@@ -50,7 +39,6 @@ function update_array_size()
 
 window.onload=update_array_size();
 
-//Running the appropriate algorithm.
 for(var i=0;i<butts_algos.length;i++)
 {
     butts_algos[i].addEventListener("click",runalgo);
@@ -70,31 +58,94 @@ function disable_buttons()
     }
 }
 
-function runalgo()
-{
-    disable_buttons();
 
-    this.classList.add("butt_selected");
-    switch(this.innerHTML)
-    {
-        case "Bubble":Bubble();
-                        break;
-        case "Selection":Selection_sort();
-                        break;
-        case "Insertion":Insertion();
-                        break;
-        case "Merge":Merge();
-                        break;
-        case "Quick":Quick();
-                        break;
-        case "Heap":Heap();
-                        break;
+
+
+
+// Function to analyze array characteristics
+function analyzeArray(arr) {
+    const n = arr.length;
+    
+    // Check if array is nearly sorted
+    let inversions = 0;
+    let sortedCount = 0;
+    for(let i = 0; i < n-1; i++) {
+        if(arr[i] <= arr[i+1]) sortedCount++;
+        if(arr[i] > arr[i+1]) inversions++;
+    }
+    
+    // Calculate characteristics
+    const nearlySorted = inversions <= n * 0.1;
+    const reverseSorted = inversions >= n * 0.9;
+    const partiallySorted = sortedCount >= n * 0.5;
+    
+    // Determine best algorithm based on characteristics
+    if(nearlySorted) {
+        return {
+            algorithm: "Insertion",
+            reason: "Array is nearly sorted - Insertion Sort performs best on nearly sorted arrays with O(n) time"
+        };
+    }
+    else if(reverseSorted) {
+        return {
+            algorithm: "Quick",
+            reason: "Array is reverse sorted - Quick Sort with good pivot selection handles this well"
+        };
+    }
+    else if(n <= 50) {
+        return {
+            algorithm: "Insertion",
+            reason: "Small array size - Insertion Sort performs well on small datasets due to low overhead"
+        };
+    }
+    else if(partiallySorted) {
+        return {
+            algorithm: "Merge",
+            reason: "Partially sorted array - Merge Sort guarantees O(n log n) regardless of input order"
+        };
+    }
+    else {
+        return {
+            algorithm: "Heap",
+            reason: "Random order array - Heap Sort provides consistent O(n log n) performance with O(1) space"
+        };
     }
 }
 
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
+// Function to update the best algorithm display
+function updateBestAlgoDisplay(selectedAlgo) {
+    const recommendation = analyzeArray(div_sizes);
+    const bestAlgoSpan = document.getElementById("best_algo");
     
-    *****************
-*/
+    if(selectedAlgo === recommendation.algorithm) {
+        bestAlgoSpan.innerHTML = `${recommendation.algorithm} (Good choice! ${recommendation.reason})`;
+        bestAlgoSpan.style.color = "#4CAF50"; // Green for good choice
+    } else {
+        bestAlgoSpan.innerHTML = `${recommendation.algorithm} would be better (${recommendation.reason})`;
+        bestAlgoSpan.style.color = "#FF5722"; // Orange for suggesting a different choice
+    }
+}
+
+// Modify the existing runalgo function to include recommendation
+function runalgo() {
+    disable_buttons();
+    this.classList.add("butt_selected");
+    
+    // Update best algorithm recommendation
+    updateBestAlgoDisplay(this.innerHTML);
+    
+    switch(this.innerHTML) {
+        case "Bubble": Bubble();
+                    break;
+        case "Selection": Selection_sort();
+                    break;
+        case "Insertion": Insertion();
+                    break;
+        case "Merge": Merge();
+                    break;
+        case "Quick": Quick();
+                    break;
+        case "Heap": Heap();
+                    break;
+    }
+}
